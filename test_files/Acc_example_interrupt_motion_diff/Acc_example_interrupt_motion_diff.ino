0b67fe02a5a42114310531b25189fb4b1a3fca6c
Gray Lunn <lunng@oregonstate.edu>
2018-07-02 15:41:00 -0700

    Config_interrupts uses less data, pulls interrupt low on interrupt

--- test_files/Acc_example_interrupt_motion_diff/Acc_example_interrupt_motion_diff.ino
+++ test_files/Acc_example_interrupt_motion_diff/Acc_example_interrupt_motion_diff.ino
@@ -114,124 +114,131 @@ void loop()
 More info about listed data registers on the LISH3DH datasheet:
 http://www.st.com/content/ccc/resource/technical/document/datasheet/3c/ae/50/85/d6/b1/46/fe/CD00274221.pdf/files/CD00274221.pdf/jcr:content/translations/en.CD00274221.pdf
 */
+
+
+/* Function: configInterrupts
+ * Use: set the necessary registers on a sparkfun LIS3DH accelerometer to send
+ *      an interrupt signal from I1
+ * Precondoitions: Device is initialized using I2C constructor
+ */
 void configIntterupts()
 {
-  uint8_t dataToWrite = 0;
-
-  //LIS3DH_CTRL_REG1   
-  dataToWrite |= 0x50;// most significant nibble controls data rate, 5 = 100Hz
-  dataToWrite |= 0x08;//Low power enable
-  dataToWrite |= 0x04;//Z enable
-  dataToWrite |= 0x02;//Y enable
-  dataToWrite |= 0x01;//X enable
-  myIMU.writeRegister(LIS3DH_CTRL_REG1, dataToWrite);
+  //uint8_t dataToWrite = 0;
   
   //LIS3DH_INT1_CFG   
   //dataToWrite |= 0x80;//AOI, 0 = OR 1 = AND
-  dataToWrite |= 0x40;//6D, 0 = interrupt source, 1 = 6 direction source
+  //dataToWrite |= 0x40;//6D, 0 = interrupt source, 1 = 6 direction source
   //Set these to enable individual axes of generation source (or direction)
   // -- high and low are used generically
   //dataToWrite |= 0x20;//Z high
   //dataToWrite |= 0x10;//Z low
-  dataToWrite |= 0x08;//Y high
+  //dataToWrite |= 0x08;//Y high
   //dataToWrite |= 0x04;//Y low
-  dataToWrite |= 0x02;//X high
+  //dataToWrite |= 0x02;//X high
   //dataToWrite |= 0x01;//X low
-  myIMU.writeRegister(LIS3DH_INT1_CFG, dataToWrite);
+  myIMU.writeRegister(LIS3DH_INT1_CFG, 0x5F);
   
   //LIS3DH_INT1_THS   
-  dataToWrite = 0;
+  //dataToWrite = 0;
   //Provide 7 bit value, 0x7F always equals max range by accelRange setting
-  dataToWrite |= 0x10; //0x10 -> 1/8 range
-  myIMU.writeRegister(LIS3DH_INT1_THS, dataToWrite);
+  //dataToWrite |= 0x10; //0x10 -> 1/8 range
+  myIMU.writeRegister(LIS3DH_INT1_THS, 0x10);
   
   //LIS3DH_INT1_DURATION  
-  dataToWrite = 0;
+  //dataToWrite = 0;
   //minimum duration of the interrupt
   //LSB equals 1/(sample rate)
-  dataToWrite |= 0x0A; // 1 * 1/50 s = 20ms
-  myIMU.writeRegister(LIS3DH_INT1_DURATION, dataToWrite);
-  
+  //dataToWrite |= 0x0A; // 1 * 1/50 s = 20ms
+  myIMU.writeRegister(LIS3DH_INT1_DURATION, 0x0A);
+
   //LIS3DH_CLICK_CFG   
-  dataToWrite = 1;
+  //dataToWrite = 0;
   //Set these to enable individual axes of generation source (or direction)
   // -- set = 1 to enable
   //dataToWrite |= 0x20;//Z double-click
-  dataToWrite |= 0x10;//Z click
+  //dataToWrite |= 0x10;//Z click
   //dataToWrite |= 0x08;//Y double-click 
-  dataToWrite |= 0x04;//Y click
+  //dataToWrite |= 0x04;//Y click
   //dataToWrite |= 0x02;//X double-click
-  dataToWrite |= 0x01;//X click
-  myIMU.writeRegister(LIS3DH_CLICK_CFG, dataToWrite);
+  //dataToWrite |= 0x01;//X click
+  myIMU.writeRegister(LIS3DH_CLICK_CFG, 0x15);
   
   //LIS3DH_CLICK_SRC
-  dataToWrite = 1;
+  //dataToWrite = 0;
   //Set these to enable click behaviors (also read to check status)
   // -- set = 1 to enable
   //dataToWrite |= 0x20;//Enable double clicks
-  dataToWrite |= 0x04;//Enable single clicks
+  //dataToWrite |= 0x04;//Enable single clicks
   //dataToWrite |= 0x08;//sine (0 is positive, 1 is negative)
-  dataToWrite |= 0x04;//Z click detect enabled
-  dataToWrite |= 0x02;//Y click detect enabled
-  dataToWrite |= 0x01;//X click detect enabled
-  myIMU.writeRegister(LIS3DH_CLICK_SRC, dataToWrite);
+  //dataToWrite |= 0x04;//Z click detect enabled
+  //dataToWrite |= 0x02;//Y click detect enabled
+  //dataToWrite |= 0x01;//X click detect enabled
+  myIMU.writeRegister(LIS3DH_CLICK_SRC, 0x07);
   
   //LIS3DH_CLICK_THS   
-  dataToWrite = 0;
+  //dataToWrite = 0;
   //This sets the threshold where the click detection process is activated.
   //Provide 7 bit value, 0x7F always equals max range by accelRange setting
-  dataToWrite |= 0x0A; // ~1/16 range
-  myIMU.writeRegister(LIS3DH_CLICK_THS, dataToWrite);
-  
+  //dataToWrite |= 0x0A; // ~1/16 range
+  myIMU.writeRegister(LIS3DH_CLICK_THS, 0x0A);
+
   //LIS3DH_TIME_LIMIT  
-  dataToWrite = 0;
+  //dataToWrite = 0;
   //Time acceleration has to fall below threshold for a valid click.
   //LSB equals 1/(sample rate)
-  dataToWrite |= 0x08; // 8 * 1/50 s = 160ms
-  myIMU.writeRegister(LIS3DH_TIME_LIMIT, dataToWrite);
+  //dataToWrite |= 0x08; // 8 * 1/50 s = 160ms
+  myIMU.writeRegister(LIS3DH_TIME_LIMIT, 0x08);
   
   //LIS3DH_TIME_LATENCY
-  dataToWrite = 0;
+  //dataToWrite = 0;
   //hold-off time before allowing detection after click event
   //LSB equals 1/(sample rate)
-  dataToWrite |= 0x08; // 8 * 1/50 s = 160ms
-  myIMU.writeRegister(LIS3DH_TIME_LATENCY, dataToWrite);
+  //dataToWrite |= 0x08; // 8 * 1/50 s = 160ms
+  myIMU.writeRegister(LIS3DH_TIME_LATENCY, 0x08);
   
   //LIS3DH_TIME_WINDOW 
-  dataToWrite = 0;
+  //dataToWrite = 0;
   //hold-off time before allowing detection after click event
   //LSB equals 1/(sample rate)
-  dataToWrite |= 0x10; // 16 * 1/50 s = 320ms
-  myIMU.writeRegister(LIS3DH_TIME_WINDOW, dataToWrite);
+  //dataToWrite |= 0x10; // 16 * 1/50 s = 320ms
+  myIMU.writeRegister(LIS3DH_TIME_WINDOW, 0x10);
 
-  //LIS3DH_CTRL_REG5
-  //Int1 latch interrupt and 4D on  int1 (preserve fifo en)
-  myIMU.readRegister(&dataToWrite, LIS3DH_CTRL_REG5);
-  dataToWrite &= 0xF3; //Clear bits of interest
-  dataToWrite |= 0x08; //Latch interrupt (Cleared by reading int1_src)
-  //dataToWrite |= 0x04; //Pipe 4D detection from 6D recognition to int1?
-  myIMU.writeRegister(LIS3DH_CTRL_REG5, dataToWrite);
+  //LIS3DH_CTRL_REG1   
+  //dataToWrite |= 0x50;// most significant nibble controls data rate, 5 = 100Hz
+  //dataToWrite |= 0x08;//Low power enable
+  //dataToWrite |= 0x04;//Z enable
+  //dataToWrite |= 0x02;//Y enable
+  //dataToWrite |= 0x01;//X enable
+  myIMU.writeRegister(LIS3DH_CTRL_REG1, 0x5F);
 
   //LIS3DH_CTRL_REG3
+  
   //Choose source for pin 1
-  dataToWrite = 0;
-  dataToWrite |= 0x80; //Click detect on pin 1
-  dataToWrite |= 0x40; //AOI1 event (Generator 1 interrupt on pin 1)
+  //dataToWrite = 0;
+  //dataToWrite |= 0x80; //Click detect on pin 1
+  //dataToWrite |= 0x40; //AOI1 event (Generator 1 interrupt on pin 1)
   //dataToWrite |= 0x20; //AOI2 event ()
   //dataToWrite |= 0x10; //Data ready
   //dataToWrite |= 0x04; //FIFO watermark
   //dataToWrite |= 0x02; //FIFO overrun
-  myIMU.writeRegister(LIS3DH_CTRL_REG3, dataToWrite);
- 
+  myIMU.writeRegister(LIS3DH_CTRL_REG3, 0xC0);
+
+  //LIS3DH_CTRL_REG5
+  //Int1 latch interrupt and 4D on  int1 (preserve fifo en)
+  //myIMU.readRegister(&dataToWrite, LIS3DH_CTRL_REG5);
+  //dataToWrite &= 0xF3; //Clear bits of interest
+  //dataToWrite |= 0x08; //Latch interrupt (Cleared by reading int1_src)
+  //dataToWrite |= 0x04; //Pipe 4D detection from 6D recognition to int1?
+  myIMU.writeRegister(LIS3DH_CTRL_REG5, 0x08);
+  
   //LIS3DH_CTRL_REG6
   //Choose source for pin 2 and both pin output inversion state
-  dataToWrite = 0;
-  dataToWrite |= 0x80; //Click int on pin 2
-  dataToWrite |= 0x40; //Generator 1 interrupt on pin 2
+  //dataToWrite = 0;
+  //dataToWrite |= 0x80; //Click int on pin 2
+  //dataToWrite |= 0x40; //Generator 1 interrupt on pin 2
   //dataToWrite |= 0x10; //boot status on pin 2
   //dataToWrite |= 0x02; //invert both outputs
-  myIMU.writeRegister(LIS3DH_CTRL_REG6, dataToWrite);
-  
+  myIMU.writeRegister(LIS3DH_CTRL_REG6, 0xC2);
 }
 
 
