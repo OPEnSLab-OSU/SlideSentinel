a0120f7471914cdc6cddaf3526b3e68e4523bbb2
Gray Lunn <lunng@oregonstate.edu>
2018-11-02 09:50:32 -0700

    Interrupt fully functional for use case

--- Devices/Adafruit MMA8451 - Accelerometer/1) Development Code/MMA8451_IntCfg/MMA8451_IntCfg.ino
+++ Devices/Adafruit MMA8451 - Accelerometer/1) Development Code/MMA8451_IntCfg/MMA8451_IntCfg.ino
@@ -14,15 +14,19 @@ Must use the updated MMA8451 Library included within this repo
 
 #define ACCEL_EN_PIN 10
 
+/* function declarations */ 
+void mmaCSVRead(Adafruit_MMA8451 device);
+void configInterrupts(Adafruit_MMA8451 device);
+void mmaSetupSlideSentinel();
 
+
+/* global variable declarations */ 
 Adafruit_MMA8451 mma = Adafruit_MMA8451();
 bool accelFlag = false;
-int dataRead;
-int counter = 0;
-unsigned long int read_time;
+unsigned long int read_time;    // timeout for reading strings as not to overload serial monitor
 sensors_event_t event; 
 
-// Interrupt service routine
+/* Interrupt service routine for accelerometer interrupts */
 void wakeUpAccel()
 {
   accelFlag = true;
@@ -35,60 +39,40 @@ void setup(void) {
   while(!Serial);   // wait for the serial monitor to open before executing any code
   
   Serial.println("Adafruit MMA8451 test!");
-  
 
-  if (! mma.begin()) {
-    Serial.println("Couldnt start");
-    while (1);
-  }
-  
-  Serial.println("MMA8451 found!");
-
-  // built in configurations
-  mma.setRange(MMA8451_RANGE_2_G);
-  mma.setDataRate(MMA8451_DATARATE_6_25HZ);
-  Serial.print("Range = "); Serial.print(2 << mma.getRange()); Serial.println("G");
-
-  while (mma.readRegister8(MMA8451_REG_CTRL_REG2) & 0x40);
-
-  mma.writeRegister8_public(MMA8451_REG_CTRL_REG3, 0x41);
-  Serial.print("dataToWrite: "); 
-  Serial.println(0x41, HEX);
-  dataRead = mma.readRegister8(MMA8451_REG_CTRL_REG3);
-  Serial.print("MMA8451_REG_CTRL_REG3: "); 
-  Serial.println(dataRead, HEX);
-
-  digitalWrite(ACCEL_EN_PIN, INPUT_PULLUP);
+  mmaSetupSlideSentinel();
   
   accelFlag = false;
   configInterrupts(mma);
+
+  digitalWrite(ACCEL_EN_PIN, INPUT_PULLUP); // pulldown interrupt
   attachInterrupt(digitalPinToInterrupt(ACCEL_EN_PIN), wakeUpAccel, LOW);
-  Serial.println("Int attached");
+  Serial.println("Interrupt attached");
+  
   mma.readRegister8(MMA8451_REG_TRANSIENT_SRC); //clear the interrupt register
   read_time = millis();
 }
 
 void loop() {
-  // Read the 'raw' data in 14-bit counts
-
+    // Read the 'raw' data in 14-bit counts, print to serial in CSV format time(ms),X,Y,Z
     if(millis() - read_time > 500 && !accelFlag){ 
-        mma.getEvent(&event);
-        Serial.print(millis()); Serial.print(","); 
-        Serial.print(mma.x); Serial.print(",");
-        Serial.print(mma.y); Serial.print(",");
-        Serial.println(mma.z);
+        mmaCSVRead(mma);
         read_time = millis();
     }
+    
+    // perform any bigger interrupt related actions here, this will just print some info to show what interrupted the accel
     if(accelFlag){
         Serial.println("Interrupt triggered");   
-        accelFlag = false;
+        accelFlag = false; // reset flag, clear the interrupt
         mma.readRegister8(MMA8451_REG_TRANSIENT_SRC); //clear the interrupt register
+        
+        // reattach the interrupt, can be done anywhere in code, but only after the interrupt has triggered and detached
         attachInterrupt(digitalPinToInterrupt(ACCEL_EN_PIN), wakeUpAccel, LOW);
     }
 }
 
-// comment/uncomment these to enable functionality
-// Transient detection donfiguration for mma accelerometer
+// comment/uncomment these to enable functionality described
+/* Transient detection donfiguration for mma accelerometer, use this format and Adafruit_MMA8451::writeRegister8_public to configure registers */
 void configInterrupts(Adafruit_MMA8451 device){
     uint8_t dataToWrite = 0;
 
@@ -101,7 +85,7 @@ void configInterrupts(Adafruit_MMA8451 device){
     //dataToWrite |= 0x10;    // Wake from Pulse function enable
     //dataToWrite |= 0x08;    // Wake from freefall/motion decect interrupt
     //dataToWrite |= 0x02;    // Interrupt polarity 1 = active high
-    //TESTdataToWrite |= 0x01;    // (0) Push/pull or (1) open drain interrupt, determines whether bus is driven by device, or left to hang
+    dataToWrite |= 0x01;    // (0) Push/pull or (1) open drain interrupt, determines whether bus is driven by device, or left to hang
 
     device.writeRegister8_public(MMA8451_REG_CTRL_REG3, dataToWrite);
 
@@ -155,5 +139,31 @@ void configInterrupts(Adafruit_MMA8451 device){
     device.writeRegister8_public(MMA8451_REG_TRANSIENT_CT, dataToWrite);
 
     dataToWrite = 0;    
+}
+
+/* Just a printing function */
+void mmaCSVRead(Adafruit_MMA8451 device){
+    device.getEvent(&event);
+    Serial.print(millis()); Serial.print(","); 
+    Serial.print(device.x); Serial.print(",");
+    Serial.print(device.y); Serial.print(",");
+    Serial.println(device.z);
+}
+
+/* Setup for mma use with Slide Sentinel, other use cases will be pretty similar */
+void mmaSetupSlideSentinel(){
+  if (! mma.begin()) {
+    Serial.println("Couldnt start");
+    while (1);
+  }
+  
+  Serial.println("MMA8451 found!");
+
+  // library configurations
+  mma.setRange(MMA8451_RANGE_2_G);
+  mma.setDataRate(MMA8451_DATARATE_6_25HZ);
+  Serial.print("Range = "); Serial.print(2 << mma.getRange()); Serial.println("G");
+
+  while (mma.readRegister8(MMA8451_REG_CTRL_REG2) & 0x40);
 }
 
