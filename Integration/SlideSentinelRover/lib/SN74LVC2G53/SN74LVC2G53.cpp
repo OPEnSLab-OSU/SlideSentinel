5ba40a5b23e29e921745078b9686e72931e3e766
Blake Hudson <hudsonbl@oregonstate.edu>
2019-11-20 13:27:34 -0800

    Updated SN74LVC2G53 Class Code by updating names to be more universal for the mux itself

--- Integration/SlideSentinelRover/lib/SN74LVC2G53/SN74LVC2G53.cpp
+++ Integration/SlideSentinelRover/lib/SN74LVC2G53/SN74LVC2G53.cpp
@@ -1,41 +1,48 @@
 #include "SN74LVC2G53.h"
 
-SN74LVC2G53::SN74LVC2G53(HardwareSerial &monitor, uint8_t switchPin, uint8_t controlSwitch)
-    : m_monitor(monitor)
-    , m_switchPin(switchPin)
+SN74LVC2G53::SN74LVC2G53(uint8_t switchPin, uint8_t controlSwitch)
+    : m_switchPin(switchPin)
     , m_controlSwitch(controlSwitch)
     {
+    if(m_controlSwitch != -1)
+        pinMode(m_controlSwitch, OUTPUT);
+
     pinMode(m_switchPin, OUTPUT);
-    pinMode(m_controlSwitch, OUTPUT);
     }
 
-void SN74LVC2G53::currentDirection(){
+void SN74LVC2G53::printDirection(){
     if(m_switchDir == 1)
-        m_monitor.println("Radio Tx to Feather Rx");
+        Serial.println("Flow between COM and Y1");
     else 
-        m_monitor.println("Radio Tx to GNSS Rx");
+        Serial.println("Flow between COM and Y2");
 }
 
 bool SN74LVC2G53::enableSwitch(){
+    if(m_controlSwitch == -1)
+        return false;
+
     digitalWrite(m_controlSwitch, LOW);
-    m_monitor.println("Mux Enabled");
+    Serial.println("Mux Enabled");
     return true;
 }
 
 bool SN74LVC2G53::disableSwitch(){
+    if(m_controlSwitch == -1)
+        return false;
+
     digitalWrite(m_controlSwitch, HIGH);
-    m_monitor.println("Mux Disabled");
+    Serial.println("Mux Disabled");
     return true;
 }
 
 void SN74LVC2G53::radioToFeather(){
     digitalWrite(m_switchPin, LOW);
     m_switchDir = 1;
-    currentDirection();
+    printDirection();
 }
 
 void SN74LVC2G53::radioToGNSS(){
     digitalWrite(m_switchPin, HIGH);
     m_switchDir = 2;
-    currentDirection();
+    printDirection();
 }
--- Integration/SlideSentinelRover/lib/SN74LVC2G53/SN74LVC2G53.h
+++ Integration/SlideSentinelRover/lib/SN74LVC2G53/SN74LVC2G53.h
@@ -9,30 +9,18 @@
 
 class SN74LVC2G53 {
     public:
-        SN74LVC2G53(HardwareSerial &monitor, uint8_t switchPin, uint8_t controlSwitch);
-        void currentDirection();
+        // If INH pin is unused and grounded set controlSwitch to -1
+        SN74LVC2G53(uint8_t switchPin, uint8_t controlSwitch);
+        void printDirection();
         bool enableSwitch();
         bool disableSwitch();
-        void radioToFeather();
-        void radioToGNSS();
+        void COMtoY1(); // Bi-Directional Flow between pins
+        void COMtoY2(); // Bi-Directional Flow between pins
 
     private:
-        HardwareSerial &m_monitor;
-        uint8_t m_switchPin;      // A Pin 2 Enables or disables the switch
+        uint8_t m_switchPin;      // A Pin 2 Switches the flow of mux between Y1 and Y2
         uint8_t m_controlSwitch;  // INH Pin 5 Controls the switch
         int m_switchDir; 
-
 };
-
-// ON PCB INH should be pin 2 GND is on 2 and 4 it should be on 3 and 4
-// ^---Followed the YZP package instead of the DCT package
-// INH high disables switches
-// INH low enables the switches
-// Allows on off control 0-Vcc or 0-5V
-// Y1 -> Feather Rx
-// Y2 -> GNSS Tx
-// COM -> Tx from radio
-// A  -> Feather A0
-// INH pin 2 needs to go to a analog pin on feather or just remain low which i think is the logic Kamron has set up with board
 
 #endif // _SN74LVC2G53_H_
