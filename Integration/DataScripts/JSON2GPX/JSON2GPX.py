8c975542436a91e8c210fc31fc491d53334b49fc
Will Richards <richawil@oregonstate.edu>
2023-02-15 18:03:54 -0800

    Helper script to convert Rover data to gpx files

---
+++ Integration/DataScripts/JSON2GPX/JSON2GPX.py
@@ -0,0 +1,32 @@
+################################################
+#
+#   JSON2GPX.py
+#   Converts Slide Sentinel Json Data into GPX Waypoints to easily be plotted in QGIS
+#   Just Run this from inside a folder of .json documents from Rovers to convert them to GPX waypoints
+#   Author: Will Richards
+#
+################################################
+
+import json
+import glob
+from gps_time.core import GPSTime
+
+for file in glob.glob("./*.json"):
+    with open(str(file[2:-5]) + ".gpx", "w") as gpxFile:
+        # GPX Boiler Plate 
+        gpxFile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+        gpxFile.write("<gpx version=\"1.0\">\n")
+        with open(file, "r") as dataFile:
+            for line in dataFile:
+                jsonPacket = json.loads(line)
+
+                # Use the gps-time library to convert our gps time into a UTC time, convert this time to the standard T,Z format
+                timeStr = str(GPSTime(week_number = int(jsonPacket["Week"]), time_of_week=int(str(jsonPacket["Seconds"])[:6])).to_datetime()).replace(" ", "T")
+                timeStr += "Z"
+
+                # Craft the waypoint object and write it to the file
+                waypoint = "\t<wpt lat=\"{0}\" lon=\"{1}\"><time>{2}</time></wpt>\n".format(jsonPacket["Latitude"], jsonPacket["Longitude"], timeStr)
+                gpxFile.write(waypoint)
+
+        # Close the GPX document
+        gpxFile.write("</gpx>")
